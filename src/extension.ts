83bda92fdc7ce9f6c0ffb7ba5273d01f7b96da8a
shohta.noda <sn.limelight@gmail.com>
2023-06-24 22:18:41 +0900

    Send a request to the OpenAI API

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { Configuration, OpenAIApi } from 'openai';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -13,10 +14,37 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vue3codegenerator.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('vue3codegenerator.helloWorld',  async () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Vue3CodeGenerator!');
+
+		const conf = vscode.workspace.getConfiguration("vue3codegenerator");
+		const apiKey = conf.get('apiKey') as string;
+		const openai = new OpenAIApi(new Configuration({ apiKey }));
+
+		let response = '';
+
+		try {
+			const completion = await openai.createChatCompletion({
+				model: 'gpt-3.5-turbo',
+				messages: [{ role: "user", content: 'こんにちは' }]
+			});
+			response = completion.data.choices[0].message?.content || '';
+		} catch (error: any) {
+			let e = '';
+			if (error.response) {
+				e = `${error.response.status} ${error.response.data.message}`;
+			} else {
+				e = error.message;
+			}
+			response += `[ERROR] ${e}`;
+		}
+
+		vscode.window.showInformationMessage(response);
+
+		vscode.window.activeTextEditor?.edit(async edit => {
+			edit.insert(new vscode.Position(2, 0), response);
+		});
 	});
 
 	context.subscriptions.push(disposable);
