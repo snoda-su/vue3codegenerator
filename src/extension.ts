0fb0fb367f0a3f4d2a2985aff7f8b335406caa82
shohta.noda <sn.limelight@gmail.com>
2023-06-25 23:46:15 +0900

    Delete unnecessary comments.

--- src/extension.ts
+++ src/extension.ts
@@ -1,22 +1,8 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { Configuration, OpenAIApi } from 'openai';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vue3codegenerator" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('vue3codegenerator.replace',  async () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
 
 		const conf = vscode.workspace.getConfiguration("vue3codegenerator");
 		const apiKey = conf.get('apiKey') as string;
@@ -56,15 +42,14 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 function createPrompt(selectedText: string) {
+	// TODO: 英語にする
 	return `# 命令書:
 	以下のコードをcompositionAPIを使った書き方に変換してください
 	
 	# コード：
 	${selectedText}
 	
-	#出力：
-	`;
+	#出力：`;
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
